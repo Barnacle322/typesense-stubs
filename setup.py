9b2d2a529d4ffb2fd2903c0ba086296b53aeb15d
Barnacle322 <arstan.usenov@gmail.com>
2024-11-14 12:08:11 +0400

    📄 Add more package info

--- setup.py
+++ setup.py
@@ -3,8 +3,19 @@ from setuptools import find_packages, setup
 setup(
     name="typesense-stubs",
     version="0.1",
+    description="Type stubs for the Typesense library",
+    author="Arstanbek Usenov",
+    author_email="arstan@globalify.xyz",
+    url="https://github.com/Barnacle322/typesense-stubs",
     packages=find_packages(),
     install_requires=[
         "typesense",
     ],
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
 )
